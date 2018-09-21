d787a6837ccc5811178ed0395c8b10f7b3304708
AngelesOct <171711o255@utectulancingo.edu.mx>
2018-09-20 21:34:29 -0500

    some init msg

---
+++ src/models/ModelBloc.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+
+/**
+ *
+ * @author Octaviano
+ */
+public class ModelBloc {
+   String path ="C:\\archivos\\texto.txt";
+   String message;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+   
+    
+    
+}//class
